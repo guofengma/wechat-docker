abb97a79f077cd57a341b0c44119cc31ffbe2a49
宋杏熠 <xingyi.song@administratordeMacBook-Pro.local>
2018-12-12 10:34:20 +0800

    add log

--- app/src/main/java/com/mihoyo/hk4e/wechat/config/RequestHandlerInterceptor.java
+++ app/src/main/java/com/mihoyo/hk4e/wechat/config/RequestHandlerInterceptor.java
@@ -5,38 +5,28 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class RequestHandlerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        String param = null;
-//        BufferedReader streamReader = new BufferedReader( new InputStreamReader(request.getInputStream(), "UTF-8"));
-//        StringBuilder responseStrBuilder = new StringBuilder();
-//        String inputStr;
-//        while ((inputStr = streamReader.readLine()) != null)
-//            responseStrBuilder.append(inputStr);
-//
-//        System.out.println(responseStrBuilder.toString());
-//        JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
-//        param= jsonObject.toJSONString();
-//        System.out.println(param);
-//
         StringBuilder sb = new StringBuilder();
         sb.append(request.getRemoteAddr())
                 .append("#")
+                .append(request.getMethod())
+                .append("#")
                 .append(request.getRequestURL())
                 .append("?")
-                .append(request.getQueryString());
+                .append(request.getQueryString()).append("#");
+
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(request.getInputStream(), "UTF-8"));
+        String inputStr;
+        while ((inputStr = streamReader.readLine()) != null)
+            sb.append(inputStr);
+
         Log.requestLog.info(sb.toString());
 
-        InputStreamReader inputStreamReader = new InputStreamReader(request.getInputStream(), "UTF-8");
-        int by = 0;
-        while ((by = inputStreamReader.read()) != -1) {
-            System.out.println((char) by);
-        }
-        inputStreamReader.close();
-//        System.out.println(request.getInputStream().toString());
         return true;
     }
 
